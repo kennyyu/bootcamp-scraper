df15eb427bd07a3a55172569aa62bebbce9abeeb
Kenny Yu <yu.kenny124@gmail.com>
2013-01-06 02:27:08 -0500

    Added csv writing capabilities.
    Made search argument required. Remove unicode characters to transform to ASCII.
    Included more comments in codes for bootcampers to understand.

--- scraper.py
+++ scraper.py
@@ -1,40 +1,101 @@
 import argparse
 import bs4
+import csv
 import urllib
-import sys
 
-AMAZON_URL = "http://www.amazon.com/s/?field-keywords="
+# URL for searching for products on amazon
+AMAZON_URL = "http://www.amazon.com/s/?keywords="
 
-argparser = argparse.ArgumentParser("Web scraper using Beautiful Soup")
+# Specify command line arguments
+argparser = argparse.ArgumentParser("Web scraper for Amazon")
 argparser.add_argument("-s", "--search", type=str, dest="search",
-                       help="search to scrape")
+                       required=True, help="search to scrape")
+argparser.add_argument("-o", "--outfile", type=str, dest="outfile",
+                       default="data.csv", help="out file for csv data")
+
+def remove_non_ascii(s):
+    """ Remove non ascii characters (e.g. unicode). """
+    return "".join(filter(lambda x: ord(x) < 128, s))
+
+class Result:
+    """ Encapsulates a result of a search query. """
+
+    def __init__(self, link, title, author, new_price, old_price):
+        self.link = link
+        self.title = title
+        self.author = author
+        self.new_price = new_price
+        self.old_price = old_price
+
+    def __str__(self):
+        return "link: %s\ntitle: %s\nauthor: %s\nnew:  %f\nold: %f" % \
+            (self.link, self.title, self.author, self.new_price, self.old_price)
 
 def is_result_div(tag):
+    """ Returns true for divs with ids starting with 'result_'. """
     if not tag.name == "div":
         return False
     return tag.has_key("id") and str(tag["id"]).startswith("result_")
 
 def get_items(html):
+    # list to keep track of our extracted values
+    results = []
+
+    # results will be in divs with ids "result_1, result_2, ..."
     soup = bs4.BeautifulSoup(html)
     result_divs = soup.find_all(is_result_div)
     for result_div in result_divs:
-        title = result_div.find(class_="productTitle")
-        new_price = result_div.find(class_="newPrice")
-        used_price = result_div.find(class_="usedNewPrice")
-        print title.a["href"]
-        print str(title.a.string).lstrip()
-        print "".join(map(lambda z: str(z.string), title.span.contents)).lstrip()
-        if new_price is not None:
-            print "new:  " + str(new_price.span.string)
-        if used_price is not None:
-            print "used: " + str(used_price.div.span.string)
-        print ""
+        title_div = result_div.find(class_="productTitle")
+        new_price_div = result_div.find(class_="newPrice")
+        used_price_div = result_div.find(class_="usedNewPrice")
+
+        # Extract the fields that we want
+        link = title_div.a["href"]
+        title = str(remove_non_ascii(title_div.a.string)).lstrip()
+
+        # Concatenate all the text elements together into a string
+        author = "".join(map(lambda z: str(remove_non_ascii(z.string)),
+                             title_div.span.contents)).lstrip()[3:]
+
+        # Remove $ sign and return amount as a float.
+        new_price = 0.
+        if new_price_div is not None:
+            new_price = float(str(new_price_div.span.string)[1:])
+        used_price = 0.
+        if used_price_div is not None:
+            used_price = float(str(used_price_div.div.span.string)[1:])
+
+        # Create a result object to encapsulate the fields that we want
+        result = Result(link, title, author, new_price, used_price)
+        results.append(result)
+    return results
+
+def write_csv(filename, results):
+    """ Write results in csv format to filename. """
+    csvfile = open(filename, "wb")
+    writer = csv.DictWriter(csvfile, 
+                            ["title", "author", "link",
+                             "new_price", "old_price"])
+    writer.writeheader()
+    for result in results:
+        writer.writerow(result.__dict__) # get dict of the object's attributes
+    csvfile.close()
+
+def main(search, outfile):
+    page = 1
+    results = []
+
+    # Keep scraping all the pages until we find a page with no results.
+    while True:
+        urlobject = urllib.urlopen(AMAZON_URL + search + "&page=" + str(page))
+        new_results = get_items(urlobject.read())
+        if len(new_results) == 0:
+            break
+        else:
+            results += new_results
+            page += 1
+    write_csv(outfile, results)
 
 if __name__ == "__main__":
     args = vars(argparser.parse_args())
-    if args["search"] is None:
-        print "must provide seach query, run with -h option for help"
-        sys.exit(-1)
-    url = AMAZON_URL + args["search"]
-    urlobject = urllib.urlopen(url)
-    get_items(urlobject.read())
+    main(search=args["search"], outfile=args["outfile"])
